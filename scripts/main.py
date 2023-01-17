c6fea711859ad98016ebe77187a0ba986e546e4b
Mehdi AGUENDICH <mehdi@audakhla.ma>
2023-01-17 14:51:58 +0100

    added pyjoke

--- scripts/main.py
+++ scripts/main.py
@@ -4,13 +4,13 @@ import re
 import pprint
 import random
 
-obj = JarvisAI.JarvisAssistant()
+Mavis = JarvisAI.JarvisAssistant()
 
 
 def t2s(text):
-    obj.text2speech(text)
+    Mavis.text2speech(text)
 
-# wakeWord = obj.mic_input()
+# wakeWord = Mavis.mic_input()
 # if re.search('jarvis wake up|jarvis I need you|jarvis', wakeWord):
 
 
@@ -40,61 +40,61 @@ def greet():
             str(datetime.now().minute)+" AM "+" now."
 
     # fetch and retuens current weather conditions for Dakhla
-    greet_text += obj.weather("dakhla")
+    greet_text += Mavis.weather("dakhla")
     return greet_text
 
 
 # executes greet function
 t2s(greet())
 
-res = obj.mic_input()
+res = Mavis.mic_input()
 
 while res:
 
-    res = obj.mic_input()
+    res = Mavis.mic_input()
 
     if re.search('weather|temperature', res):
         city = res.split(' ')[-1]
-        weather_res = obj.weather(city=city)
+        weather_res = Mavis.weather(city=city)
         print(weather_res)
         t2s(weather_res)
 
     if re.search('news', res):
-        news_res = obj.news()
+        news_res = Mavis.news()
         pprint.pprint(news_res)
-        t2s(f"I have found {len(news_res)} news. You can read it. Let me tell you first 2 of them")
+        t2s(f"I have found {len(news_res)} news. You can read it. Let me read you the first 2 lines")
         t2s(news_res[0])
         t2s(news_res[1])
 
     if re.search('tell me about', res):
         topic = res.split(' ')[-1]
-        wiki_res = obj.tell_me(topic)
+        wiki_res = Mavis.tell_me(topic)
         print(wiki_res)
         t2s(wiki_res)
 
     if re.search('date', res):
-        date = obj.tell_me_date()
+        date = Mavis.tell_me_date()
         print(date)
         t2s(date)
 
     if re.search('time', res):
-        time = obj.tell_me_time()
+        time = Mavis.tell_me_time()
         print(time)
         t2s(time)
 
-    if re.search('jokes', res):
-        obj.jokes()
+    if re.search('joke|jokes', res):
+        Mavis.tell_me_Joke()
 
     if re.search('search in youtube for', res) or re.search('searching in youtube for', res) or re.search('search youtube for', res):
         search = res.split(' ')[-1]
         t2s("searching for "+search+" in youtube")
-        open_result = obj.website_opener(
+        open_result = Mavis.website_opener(
             "youtube.com/results?search_query="+search)
 
     if re.search('search in google for', res) or re.search('searching in google for', res) or re.search('search google for', res) or re.search('google search for', res):
         search = res.split(' ')[-1]
         t2s("searching for "+search+" in google")
-        open_result = obj.website_opener(
+        open_result = Mavis.website_opener(
             "google.com/results?search_query="+search)
 
     if re.search('open directory name', res):
@@ -110,15 +110,15 @@ while res:
             print('dir path not found')
         else:
             t2s('opening: ' + dir)
-            obj.open_any_dir(path)
+            Mavis.open_any_dir(path)
 
     if re.search('open', res):
         domain = res.split(' ')[-1]
         t2s("Opening "+domain)
         if domain == "facebook" or "youtube" or "gmail":
-            open_result = obj.website_opener(domain + ".com")
+            open_result = Mavis.website_opener(domain + ".com")
         else:
-            open_result = obj.website_opener(domain)
+            open_result = Mavis.website_opener(domain)
             print(open_result)
 
     if re.search('launch', res):
@@ -135,7 +135,7 @@ while res:
             print('Application path not found')
         else:
             t2s('Launching: ' + app)
-            obj.launch_any_app(path_of_app=path)
+            Mavis.launch_any_app(path_of_app=path)
 
     if re.search('close', res):
         dict_app = {
@@ -151,7 +151,7 @@ while res:
             print('Application already closed')
         else:
             t2s('Closing: ' + app)
-            obj.close_any_app(path_of_app=path)
+            Mavis.close_any_app(path_of_app=path)
 
     if re.search('hello', res):
         print('Hi')
@@ -164,17 +164,17 @@ while res:
         t2s(f"I am {response}")
 
     if re.search('what is your name|who are you', res):
-        print("My name is Jarvis, I am your personal assistant")
-        t2s("My name is Jarvis, I am your personal assistant")
+        print("My name is Mavis, I am your personal assistant")
+        t2s("My name is Mavis, I am your personal assistant")
 
     if re.search('initiate standby mode|standbymode', res):
-        obj.standbyMode()
+        Mavis.standbyMode()
 
     if re.search('shut down', res):
-        obj.shutdown()
+        Mavis.shutdown()
 
     if re.search('open my gmail', res):
-        open_result = obj.website_opener("gmail.com")
+        open_result = Mavis.website_opener("gmail.com")
         print(open_result)
 
     if re.search('what can you do', res):
@@ -183,8 +183,8 @@ while res:
             "time": "Example: 'what time it is?'",
             "date": "Example: 'what date it is?'",
             "launch applications": "Example: 'launch chrome'",
-            "tell me": "Example: 'tell me about India'",
-            "weather": "Example: 'what weather/temperature in Mumbai?'",
+            "tell me": "Example: 'tell me about Morocco'",
+            "weather": "Example: 'what weather/temperature in Rabat?'",
             "news": "Example: 'news for today' ",
         }
         ans = """I can do lots of things, for example you can ask me time, date, weather in your city,
